5c51dc075b38832e33bbf48dd24b0025550025a9
Kiran <kiranks1998@gmail.com>
2020-12-18 14:56:47 +0530

    popup login page designing

--- members/views.py
+++ members/views.py
@@ -29,7 +29,7 @@ def UserLoginView(request):
         else:
             print("error....")
             
-    return render(request, "members/login.html",context)
+    return render(request, "realbuy_app/base.html",context)
 
     
 class UserEditView(generic.UpdateView):
