914149e7794cd9e9e41faaac6b1a4cf3a3a1d930
Kiran <kiranks1998@gmail.com>
2020-12-22 16:45:25 +0530

    profile page designing

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
 from .views import UserRegisterView,UserEditView
-app_name = 'members'
+
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
