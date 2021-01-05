9b89c9ce4a0b4d66250bda0f69c18a47b57dfe48
Kiran <kiranks1998@gmail.com>
2021-01-05 20:47:19 +0530

    commenting local database

--- realbuy/settings.py
+++ realbuy/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'realbuy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -82,7 +82,7 @@ DATABASES = {
         'PORT': '5432',
    }
  }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
