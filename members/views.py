871903f73f7a6a1f787f7c54bbfd6335d0c6ef48
Kiran <kiranks1998@gmail.com>
2021-02-25 19:22:29 +0530

    showing register errors within popup using ajax

--- members/views.py
+++ members/views.py
@@ -8,62 +8,34 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 User = get_user_model()
 
 
 def UserRegisterView(request):
-    form = RegisterForm(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('login_page')
-    else:
-        print(form.errors)
-        context = {'form': form }
-        #return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-        return render(request, "realbuy_app/unsuccess.html",context)
+    if request.is_ajax():
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_page')
+        else:
+            print(form.errors)
+            context = {'form': form }
+            data = form.errors.as_json()
+            print(data)
+            return JsonResponse({
+                 'message': 'error','registerform': data
+            })
+            #return render(request, "realbuy_app/unsuccess.html",context)
     
-# def UserLoginView(request):
-#     form = LoginForm()
-#     context = {
-#         'form': form
-#     }
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST or None)
-#         print(request.user.is_authenticated)
-#         if form.is_valid():
-#             print(form.cleaned_data)
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(request, username=username, password=password)
-#             if user is None:
-#                 User = get_user_model()
-#                 user_queryset = User.objects.all().filter(email__iexact=username)
-#                 if user_queryset:
-#                     username = user_queryset[0].username
-#                     user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request,user)
-                
-#                 return redirect('add1')
-#             else:
-#                 print("error....")
-#         else:
-#             print(form.errors)
-#             context = {'loginform': form }
-#             return render(request, "realbuy_app/unsuccess.html",context)
-            
-#     else:    
-#         return render(request, "members/login.html",context)    
-
-
-
 def UserLoginView(request):
     form = LoginForm()
     context = {
         'form': form
     }
-    if request.method == 'POST':
+    if request.is_ajax():
         form = LoginForm(request.POST or None)
         print(request.user.is_authenticated)
         if form.is_valid():
@@ -84,23 +56,25 @@ def UserLoginView(request):
             else:
                 print("error....")
         else:
-            print(form.errors)
-            # for field in form:
-            #     for problem in field.errors():
-            #         messages.error(request, problem)
-
-            # for problem in form.non_field_errors():
-            #     messages.error(request, problem)
-
             
-            #messages.error(request, form)
-            #return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-            url = "{0}/{1}".format(request.META.get('HTTP_REFERER', '/'), {'form':form})
+            print(form.errors)
+            # data = model_to_dict(form)
+            # print(data)
 
-            return HttpResponseRedirect(url)
+            # data = model_to_dict(form, fields=[field.name for field in form._meta.fields])
+            # print(data)
+
+            #data = form.values()[0]
+
+            data = form.errors.as_json()
+            print(data)
+            return JsonResponse({
+                 'message': 'error','loginform': data
+            })
             
     else:    
-        return render(request, "members/login.html",context) 
+        return render(request, "members/login.html",context)    
+
     
 
 class PasswordsChangeView(PasswordChangeView):
